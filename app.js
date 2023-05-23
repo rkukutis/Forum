d61637c965d959134c652d3ff6855e235c0fd4f9
InfGibus <ricxas30@gmail.com>
2023-05-23 20:30:35 +0300

    PostgreSQL DB

--- app.js
+++ app.js
@@ -5,16 +5,8 @@ const express = require('express');
 const morgan = require('morgan');
 
 const userRoutes = require('./routes/userRoutes');
-const postRoutes = require('./routes/postRoutes');
-const AppError = require('./routes/utils/AppError');
-const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
-
-// Middlewares
-// Middleware has access to the request and response
-// next() must always be called in the end of the middleware function
-// The order of the middleware in the code is VERY important
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(express.json());
@@ -31,18 +23,10 @@ app.get('/', (req, res) => {
 
 // Mounting routers
 app.use('/users', userRoutes);
-app.use('/posts', postRoutes);
 
 // Redirects unhandled routes to root
 app.get('*', (req, res) => {
   res.end();
 });
-
-app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
-  // if next() receives an argument, express assumes an error has occured
-});
-
-app.use(globalErrorHandler);
 
 module.exports = app;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,5 @@
 const express = require('express');
-const userController = require('../controllers/userController');
-const authController = require('../controllers/authController');
 
 const router = express.Router();
-
-// User routes
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-
-router.route('/').get(userController.getAllUsers);
-router
-  .route('/:id')
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
 
 module.exports = router;
