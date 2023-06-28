f6242331ca2f660fc456ecfdef851ee42644c547
InfGibus <ricxas30@gmail.com>
2023-06-28 19:04:50 +0300

    Added bogus user image resizing

--- img/users/downloadUserImages.js
+++ img/users/downloadUserImages.js
@@ -1,22 +1,28 @@
 const axios = require('axios');
+const { Image } = require('image-js');
 const fs = require('fs');
 
 async function downloadImage(url, filename) {
-  const response = await axios.get(url, { responseType: 'arraybuffer' });
-
-  fs.writeFile(`${__dirname}/${filename}`, response.data, (err) => {
-    if (err) throw err;
-    console.log('Image downloaded successfully!');
-  });
+  try {
+    // GET image
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    const image = await Image.load(response.data);
+    image.resize({ width: 100, height: 100 }).save(filename);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
 }
 
+// Create Array [100]
 const seq = Array.from(Array(100).keys());
+// Downloads 100 generated person images
 seq.forEach((userNumber, i) => {
   setTimeout(
     () =>
       downloadImage(
         'https://thispersondoesnotexist.com/',
-        `user_${userNumber}.jpg`
+        `user_${userNumber + 1}.jpg`
       ),
     i * 2000
   );
